8bb3d25ece74e4ff9cb8aebdd9c91790d9e338cc
babacicemir <130359741+babacicemir@users.noreply.github.com>
2025-01-05 00:34:14 +0100

    added feature block user for admin

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express")
 const userRouter = require("./users")
+const adminRouter = require("./admin")
 
 const router = Router()
 
 router.use("/users", userRouter);
+router.use("/admin", adminRouter);
 
 module.exports = router
