addd5d778cfdd331b8b2da96d4155998d65c18ce
babacicemir <130359741+babacicemir@users.noreply.github.com>
2025-01-05 00:44:31 +0100

    added create user for admin

--- backend/src/routes/admin/index.js
+++ backend/src/routes/admin/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const Admin = require("../../controllers/admin")
+const User = require("../../controllers")
 
 const router = Router();
 
 router.put("/block/:id", Admin.blockUser);
 router.put("/unblock/:id", Admin.unblockUser);
+router.post("/create", User.signup);  //ovde je iskoristena funkcija za signup, kako se ne bi ista funkcija dva puta ponavljala
 module.exports = router;
