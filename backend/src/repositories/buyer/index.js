aac96fdaf5937a9a68a590b77ff4ce32aad48996
babacicemir <130359741+babacicemir@users.noreply.github.com>
2025-01-24 01:10:58 +0100

    added feature to save and delete saved ads

--- backend/src/repositories/buyer/index.js
+++ backend/src/repositories/buyer/index.js
@@ -1,19 +1,36 @@
-const { pool } = require("../../config/database")
+const { pool } = require('../../config/database')
 
 const getAllAds = async() => {
-    const query = 'SELECT * FROM ads'
-    const result = await pool.query(query)
-    return result.rows
+  const query = 'SELECT * FROM ads'
+  const result = await pool.query(query)
+  return result.rows
 }
 
 const reportedUser = async(reportInformations) => {
-    const query = 'INSERT INTO reports(userid, reported_user_id, reason, details) VALUES($1, $2, $3, $4) RETURNING*'
-    const values = [reportInformations.userId, reportInformations.reportedUserId, reportInformations.reason, reportInformations.details]
+  const query = 'INSERT INTO reports(userid, reported_user_id, reason, details) VALUES($1, $2, $3, $4) RETURNING*'
+  const values = [reportInformations.userId, reportInformations.reportedUserId, reportInformations.reason, reportInformations.details]
+  const result = await pool.query(query, values)
+  return result.rows[0]
+}
+
+const saveAd = async(informations) => {
+  const query = 'INSERT INTO saved_ads(user_id, ad_id) VALUES($1, $2) RETURNING*'
+  const values = [informations.userId, informations.adId]
+  const result = await pool.query(query, values)
+  return result.rows[0]
+}
+
+const deleteSavedAdByIds = async(adInformations) => {
+    console.log(adInformations)
+    const query = 'DELETE from saved_ads WHERE user_id=$1 AND ad_id=$2 RETURNING*'
+    const values = [adInformations.userId, adInformations.adId]
     const result = await pool.query(query, values)
     return result.rows[0]
-  }
+}
 
 module.exports = {
-    getAllAds,
-    reportedUser,
+  getAllAds,
+  reportedUser,
+  saveAd,
+  deleteSavedAdByIds,
 }
--- backend/src/routes/buyer/index.js
+++ backend/src/routes/buyer/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
 const { checkJWT, checkAccess } = require('../../middlewares')
-const Buyer = require("../../controllers/buyer")
+const Buyer = require('../../controllers/buyer')
 
 const router = Router()
 
 router.get('/ads', Buyer.allAds)
 router.post('/user/report/:id', checkJWT, checkAccess('buyer'), Buyer.reportUser )
+router.post('/ad/save/:id', checkJWT, checkAccess('buyer'), Buyer.saveAd)
+router.delete('/ad/saved/delete/:id', checkJWT, checkAccess('buyer'), Buyer.deleteSavedAd)
 
 
 module.exports = router
